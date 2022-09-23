8ede63708fd5aea05f6985a596c57152c1868dc7
Patryk Słowiński <patryk_slowinski@o2.pl>
2022-09-23 18:20:22 +0200

    Add Redux and sending data from redux to components

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,8 +1,11 @@
 import style from './Column.module.scss'
 import CardForm from '../CardForm/CardForm';
 import Card from '../Card/Card';
+import { useSelector } from 'react-redux';
 
 const Column = props => {
+
+    const cards = useSelector(state => state.cards.filter(card => card.columnId === props.id));
     return (
         <article className={style.column}>
             <h1 className={style.title}>
@@ -10,7 +13,7 @@ const Column = props => {
                 {props.title}
             </h1>
             <ul className={style.cards}>
-                {props.cards.map(card => <Card key={card.id} title={card.title} />)}
+                {cards.map(card => <Card key={card.id} title={card.title} />)}
             </ul>
             <CardForm columnId={props.id} action={props.addCard} />
         </article>
