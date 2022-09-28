33a51d98a456900eb38a19ac9b2fe86c58055224
Patryk Słowiński <patryk_slowinski@o2.pl>
2022-09-28 10:06:57 +0200

    Add option search

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,17 +1,35 @@
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
 import styles from './SearchForm.module.scss';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { useState } from 'react';
+
 
 
 const SearchForm = () => {
+
+    
+    const [searchValue, setsearchValue] = useState();
+    console.log(searchValue);
+
+
+    const handleSubmit = e => {
+        e.preventDefault();  
+        dispatch({ type: 'UPDATE_SEARCHSTRING', payload: searchValue });
+
+    }
+    const dispatch = useDispatch();
+
+
     return (
-        <form className={styles.searchForm}>
-            <TextInput placeholder='Search…' />
+        <form className={styles.searchForm} onChange={handleSubmit}>
+            <TextInput placeholder='Search…' onChange={ e => setsearchValue(e.target.value)} value={searchValue}/>
             <Button>
                 <span className="fa fa-search" />
             </Button>
         </form>    
     );
-  };
+  }
 
   export default SearchForm;
